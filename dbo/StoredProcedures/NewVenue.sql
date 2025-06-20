00076abec7ae8753104fc1efd66639aef7e32148
Rauno Voolaid <rauno.voolaid@softwerk.ee>
2025-06-20 16:22:36 +0300

    remove comment

--- dbo/StoredProcedures/NewVenue.sql
+++ dbo/StoredProcedures/NewVenue.sql
@@ -17,7 +17,6 @@
           RAISERROR ('Error. @VenueName must be specified', 11, 1)
           RETURN 1
       END
-      /*TEST TRIGGER YAML*/
   
       -- Insert Venue
       INSERT INTO [dbo].Venues
